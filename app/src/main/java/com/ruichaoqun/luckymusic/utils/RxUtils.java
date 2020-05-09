077294f4855ff4bfb2f589d2652fd2edcc210dcf
ruichaoqun <ruichaoqun121@163.com>
2020-05-09 18:06:56 +0800

    完善

--- app/src/main/java/com/ruichaoqun/luckymusic/utils/RxUtils.java
+++ app/src/main/java/com/ruichaoqun/luckymusic/utils/RxUtils.java
@@ -1,11 +1,17 @@
 package com.ruichaoqun.luckymusic.utils;
 
+import android.util.Log;
+
 import com.ruichaoqun.luckymusic.data.bean.BaseResponse;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import io.reactivex.Observable;
 import io.reactivex.ObservableSource;
 import io.reactivex.ObservableTransformer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.functions.Consumer;
 import io.reactivex.functions.Function;
 import io.reactivex.schedulers.Schedulers;
 
@@ -25,4 +31,55 @@ public class RxUtils {
             return Observable.error(new Throwable());
         };
     }
+
+//    /**
+//     * 统一线程处理
+//     * @param <T> 指定的泛型类型
+//     * @return ObservableTransformer
+//     */
+//    public static <T>  ObservableTransformer<BaseResponse<T>, BaseResponse<T>> tokenTimeoutHelper() {
+//        return new ObservableTransformer<BaseResponse<T>, BaseResponse<T>>() {
+//            @Override
+//            public ObservableSource<BaseResponse<T>> apply(Observable<BaseResponse<T>> observable) {
+//                return observable.flatMap(new Function<BaseResponse<T>, ObservableSource<BaseResponse<T>>>() {
+//                    @Override
+//                    public ObservableSource<BaseResponse<T>> apply(BaseResponse<T> t) throws Exception {
+//                        if (t != null && t.getErrorCode() == 50000) {
+//                            Log.w("QQQQQQ", "token失效");
+//                            return Observable.error(new TokenExpiredException());
+//                        }
+//                        return Observable.just(t);
+//                    }
+//                })
+//                        .retryWhen(throwableObservable -> throwableObservable.flatMap(new Function<Throwable, ObservableSource<?>>() {
+//                            @Override
+//                            public ObservableSource<?> apply(Throwable throwable) throws Exception {
+//                                if (throwable instanceof TokenExpiredException) {
+//                                    String macId = SharedPreUtil.getString(ApiConstant.ReqKey.COMM_MAC, "");
+//                                    Map<String,String> macMap = new HashMap<>();
+//                                    macMap.put("inquiryParam",macId);
+//                                    return RetrofitJavaUtil.getRetrofitUtil().create(AnychatRequestApi.class)
+//                                            .emcLogin(macMap)
+//                                            .subscribeOn(Schedulers.io())
+//                                            .doOnNext(new Consumer<BaseBean<LoginResultResp>>() {
+//                                                @Override
+//                                                public void accept(BaseBean<LoginResultResp> loginResultRespBaseBean) throws Exception {
+//                                                    Log.w("QQQQQQ","重新获取token成功"+loginResultRespBaseBean.getData().getAccess_token());
+//                                                    CacheData.setLoginResp(loginResultRespBaseBean.getData());
+//                                                    CacheData.setLogin(true);
+//                                                    EventBus.getDefault().post(new RefreshDocListEvent());
+//                                                }
+//                                            });
+//                                }
+//                                return  Observable.error(throwable);
+//                            }
+//                        }));
+//            }
+//        };
+//    }
+
+    static class TokenExpiredException extends Exception{
+
+    }
+
 }
